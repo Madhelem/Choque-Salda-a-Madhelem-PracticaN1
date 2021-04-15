bebbdcd30b000c2b53f2a4a47365e800aa95832d
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 01:33:40 -0400

    PROBLEMA10

--- src/app.js
+++ src/app.js
@@ -1,29 +1,48 @@
-/*Problema 8:
-Dado un numero N encontrar los N primeros números primos*/
-var c = 9;
-var aux = 2;
-var cont = 0;
-while (aux >= 2) {
-    var numero = primo(aux);
-    if (numero) {
-        console.log(aux);
-        cont = cont + 1;
-    }
-    if (cont == c) {
-        break;
-    }
-    aux = aux + 1;
-}
-function primo(numero) {
-    for (var i = 2; i < numero; i++) {
-        if (numero % i == 0) {
-            return false;
+/*Problema 10:
+Lea la documentación sobre la estructura de una dirección IPv4.
+Posteriormente realice un programa para determinar si una dirección determinada (dado como un string) cumple con todas las reglas de las direcciones IPv4.*/
+//-----10------------
+var browserType = '18.18.40.14';
+var num = browserType.length;
+var inicial = 0;
+var punt = 0;
+if (num <= 15 && num >= 7) {
+    for (var i = 0; i <= num; i++) {
+        var variable = browserType[i];
+        var obj = ".";
+        if (variable == obj) {
+            punt = punt + 1;
+            if (punt > 3) {
+                console.log("tiene mas de tres puntos no es una direccion ip");
+                break;
+            }
+            else {
+                var final = i;
+                var letra = browserType.substring(inicial, final);
+                var numero = parseInt(letra);
+                if (numero <= 255 && numero >= 0) {
+                    inicial = final + 1;
+                }
+                else {
+                    console.log(false);
+                    break;
+                }
+            }
         }
-    }
-    if (numero < 2) {
-        return false;
-    }
-    else {
-        return true;
+        if (i == num) {
+            var letra = browserType.substring(inicial, i);
+            var numero = parseInt(letra);
+            if (numero <= 255 && numero >= 0) {
+                if (punt < 3) {
+                    console.log(false);
+                }
+                else {
+                    console.log(true);
+                }
+            }
+            else {
+                console.log(false);
+            }
+        }
     }
 }
