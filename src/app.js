a79896a6487219ad34a80175f666cfde1142166e
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 01:21:08 -0400

    PROBLEMA5

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
-/*Problema 4:
-Dado un array de enteros, encontrar un par de elementos adyacentes tales que tengan el producto más grande*/
-var arrays = new Array();
-arrays = [3, 6, -2, -5, 7, 3];
-var sum = 0;
-var may = 0;
+/*Problema 5:
+A Pieck le entregaron un montón de estatuas de diferentes tamaños, Pieck quiere ordenarlas de la más pequeña a las mas grande sin embargo también desea que cada estatua sea mas grande que la anterior solo por 1 de diferencia por lo que también ve pertinente adquirir nuevas estatuas. Ayúdala a determinar cuántas estatuas MAS debe adquirir para lograr su cometido.*/
+var arrays = new Array;
+var arrays1 = new Array;
+arrays1 = [];
+arrays = [6, 2, 3, 8];
+var men = arrays[0];
+var may = arrays[0];
 for (var i = 0; i < arrays.length; i++) {
-    for (var j = i; j <= i; j++) {
-        sum = arrays[i] * arrays[j + 1];
-        //console.log(sum);
+    if (arrays[i] < men) {
+        men = arrays[i];
     }
-    //console.log(sum);
-    if (sum > may) {
-        may = sum;
+    else {
+        if (arrays[i] > may) {
+            may = arrays[i];
+        }
     }
 }
-console.log(may);
+while (men <= may) {
+    arrays1.push(men);
+    men++;
+}
+var l1 = arrays.length;
+var l2 = arrays1.length;
+console.log(l2 - l1);
