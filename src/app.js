b63564b3f016d220ef65ababbf6ab1673fbb4638
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 01:17:23 -0400

    PROBLEMA4

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-/*Problema 3:
-Dada una cadena comprobar si es palíndromo*/
-var palabra = 'this is a sample string';
-console.log(esPalindromo(palabra));
-function esPalindromo(palabra) {
-    palabra = palabra.toLowerCase();
-    var j = palabra.length - 1;
-    for (var i = 0, j_1 = palabra.length - 1; i <= j_1; i++, j_1--) {
-        if (palabra.charAt(i) != palabra.charAt(j_1)) {
-            return false;
-        }
+/*Problema 4:
+Dado un array de enteros, encontrar un par de elementos adyacentes tales que tengan el producto más grande*/
+var arrays = new Array();
+arrays = [3, 6, -2, -5, 7, 3];
+var sum = 0;
+var may = 0;
+for (var i = 0; i < arrays.length; i++) {
+    for (var j = i; j <= i; j++) {
+        sum = arrays[i] * arrays[j + 1];
+        //console.log(sum);
     }
-    return true;
+    //console.log(sum);
+    if (sum > may) {
+        may = sum;
+    }
 }
+console.log(may);
