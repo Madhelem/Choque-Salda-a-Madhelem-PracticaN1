10c8e14f7ef2c4d2a851d75db3dbe21f65b47f6d
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 01:15:43 -0400

    PROBLEMA2

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
-/*Problema 1:
-Dado un numero n determinar si es primo o no*/
-//let n:number = prompt("Introdusca un valor a calcular");
-var j = 21;
-console.log(primo(j));
-//console.log(primo(n));
-function primo(numero) {
-    for (var i = 2; i < numero; i++) {
-        if (numero % i == 0) {
-            return false;
-        }
-    }
-    if (numero < 2) {
-        return false;
-    }
-    else {
-        return true;
-    }
+/*Problema 2:
+Dada una cadena mostrarla invertida*/
+var cadena = 'this is a sample string';
+var cadenarevertida = "";
+for (var i = cadena.length - 1; i >= 0; i--) {
+    cadenarevertida += cadena[i];
 }
+console.log(cadenarevertida);
