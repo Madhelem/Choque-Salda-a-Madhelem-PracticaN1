295a39cfe434f774b6bcdbf53bd39414c7905f79
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 01:16:35 -0400

    PROBLEMA3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-/*Problema 2:
-Dada una cadena mostrarla invertida*/
-var cadena = 'this is a sample string';
-var cadenarevertida = "";
-for (var i = cadena.length - 1; i >= 0; i--) {
-    cadenarevertida += cadena[i];
+/*Problema 3:
+Dada una cadena comprobar si es palíndromo*/
+var palabra = 'this is a sample string';
+console.log(esPalindromo(palabra));
+function esPalindromo(palabra) {
+    palabra = palabra.toLowerCase();
+    var j = palabra.length - 1;
+    for (var i = 0, j_1 = palabra.length - 1; i <= j_1; i++, j_1--) {
+        if (palabra.charAt(i) != palabra.charAt(j_1)) {
+            return false;
+        }
+    }
+    return true;
 }
-console.log(cadenarevertida);
